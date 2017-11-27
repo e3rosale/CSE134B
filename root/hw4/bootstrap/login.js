a2491031335ac072cc4e5621a02d338a456fcda6
Eduardo <eduardo.d.rosales1@gmail.com>
2017-11-26 21:47:19 -0800

    finished bootstrap login

---
+++ root/hw4/bootstrap/login.js
@@ -0,0 +1,29 @@
+var registered_users = localStorage;
+function login_user() {
+  var login_email = document.querySelector('#Email').value;
+  var login_password = document.querySelector('#Password').value;
+
+  if (login_email == "" || login_password == "") {
+    alert("complete all form fields");
+  } else {
+    var is_registered = false;
+    for (users in registered_users) {
+      var retrievedObject = registered_users.getItem(users);
+      var user = JSON.parse(retrievedObject);
+      if (user.email == login_email && user.password == login_password) {
+        is_registered = true;
+        break;
+      }
+    }
+    if (!is_registered) {
+      alert("This email is not registered, please proceed to register page");
+    } else {
+      alert("about to replace");
+      window.location.replace("https://www.w3schools.com");
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#login_button').addEventListener('click', function() {login_user();}, false);
+}, false);
