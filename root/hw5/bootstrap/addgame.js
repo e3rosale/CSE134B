6cc0133166ddf9d236907a471b92f77b0d83ea6a
Gustavo Carbone <gcarbone@ucsd.edu>
2017-12-10 00:35:56 -0800

    done with DB schedule

--- root/hw5/bootstrap/addgame.js
+++ root/hw5/bootstrap/addgame.js
@@ -25,19 +25,6 @@ var awayBoolean = false;
 
 if(gameToAdd !== null) {
 	gameToAdd.addEventListener('submit', saveGame, false);
-}
-
-if(gameToEdit !== null) {
-		gameToEdit.addEventListener('submit', editGameCurr, false);
-}
-
-//Getting values from form
-	var currOponent;
-	var currLocation;
-	var currDate;
-	var gameId;
-	var currStatus;
-	var currStatusValue;
 
 	db.collection('games').doc('null').set({
    		oponent: '',
@@ -52,6 +39,34 @@ if(gameToEdit !== null) {
 	}).catch(function(error) {
     	console.error('Error removing document: ', error);
 	});
+}
+
+if(gameToEdit !== null) {
+		gameToEdit.addEventListener('submit', editGameCurr, false);
+
+		db.collection('games').doc('null').set({
+   		oponent: '',
+   		location: '',
+   		date: '',
+  		status: '',
+   		id: '',
+	});
+
+	db.collection('games').doc('null').delete().then(function() {
+    	console.log('Document successfully deleted!');
+	}).catch(function(error) {
+    	console.error('Error removing document: ', error);
+	});
+}
+
+//Getting values from form
+	var currOponent;
+	var currLocation;
+	var currDate;
+	var gameId;
+	var currStatus;
+	var currStatusValue;
+
 /* 
  * Function that saves a new game to the local storage
  */
@@ -68,18 +83,18 @@ function saveGame(e) {
 		}
 	}
 
-	//Creating object to store in database
-	db.collection('games').doc(gameId).set({
+	var game = {
 		oponent: currOponent,
 		location: currLocation,
 		date: currDate,
-		status: currStatusValue, 
-		id: gameId 
-	})
-	.then(function() {
+		status: currStatusValue,
+		id: gameId
+	};
+
+	//Creating object to store in database
+	db.collection('games').doc(gameId).set(game).then(function() {
 		console.log('Success');
-	})
-	.catch(function(error) {
+	}).catch(function(error) {
 		console.error(error);
 	});
 
@@ -96,30 +111,30 @@ function saveGame(e) {
  */
 function fetchSchedule() {
 
-	// Getting objects from local storage
-	var schedule = JSON.parse(localStorage.getItem('schedule'));
 	var scheduleTable = document.getElementById('scheduleTable');
 	var ourTeam = 'Our Team';
 	scheduleTable.innerHTML = '';
 
-	// Populating table
-	if (schedule != null) {
-		for (let i = 0; i < schedule.length; i++) {
-			// var id = roster[i].id;
-			var oponent = schedule[i].oponent;
-			var location = schedule[i].location;
-			var status = schedule[i].status;
-			var date = schedule[i].date;
+	db.collection("games").get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            var oponent = doc.data().oponent;
+			var location = doc.data().location;
+			var status = doc.data().status;
+			var date = doc.data().date;
 
 			scheduleTable.innerHTML += 	'<tr><th scope="row"><time datetime="' + date + '">' + date + '</time></th>' +
 				      						'<td>' + ourTeam + '<br> vs <br>' + oponent + '</td>' +
 				      						'<td>' + location + '</td>' +
 				      						'<td>' + status + '</td>' +
-				      						'<td><a href="editgameBootstrap.html" type="button" onclick="editGame(\''+ schedule[i].id +'\')"><span class="glyphicon glyphicon-pencil"></span></a></td>' +
-				      						'<td><input type="checkbox" name="games" id="\''+ schedule[i].id +'\'"></td>' +
+				      						'<td><a href="editgameBootstrap.html" type="button" onclick="editGame(\''+ doc.data().id +'\')"><span class="glyphicon glyphicon-pencil"></span></a></td>' +
+				      						'<td><input type="checkbox" name="games" id="\''+ doc.data().id +'\'"></td>' +
 				    					'</tr>';
-		}
-	}
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
 }
 
 
@@ -135,7 +150,7 @@ function deleteGames() {
 		}
 	} 
 
-	fetchSchedule();
+	setTimeout( function(){fetchSchedule();} , 1000 );
 }
 
 
@@ -143,51 +158,60 @@ function deleteGames() {
  * 	Deletes a games by id
  */
 function deleteItemByKey(id) {
-	var schedule = JSON.parse(localStorage.getItem('schedule'));
-  
-	for (let i = 0; i < schedule.length; i++) {
-		var str = id.substring(1, id.length - 1);
-		if (schedule[i].id === str) {
-			schedule.splice(i,1);
-			break;
-		}
-	}
-  
-	localStorage.setItem('schedule', JSON.stringify(schedule));  
+
+	db.collection("games").get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            var str = id.substring(1, id.length - 1);
+			if (doc.data().id === str) {
+				db.collection("games").doc(doc.data().id).delete().then(function() {
+				    console.log("Document successfully deleted!");
+				}).catch(function(error) {
+				    console.error("Error removing document: ", error);
+				});
+			}
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    }); 
 }
 
 
 /* 
  * 	Gets the game to be edited and saves it to local storage
  */
-function editGame(id) {
-	var schedule = JSON.parse(localStorage.getItem('schedule'));
-	for (let i = 0; i < schedule.length; i++) {
-		if (schedule[i].id === id) {
-			localStorage.setItem('editGame', JSON.stringify(schedule[i]));
-			break;
-		}
-	}
+function editGame(id) {	
+	localStorage.setItem('editGameID', id);
 }
-
 
 /* 
  *	Populates the edit page with the data of the game to be edited 
  */
 function populateEdit() {
-	editThisGame = JSON.parse(localStorage.getItem('editGame'));	
 
-	document.getElementById('oponentEdit').value = editThisGame.oponent;
-	document.getElementById('locationEdit').value = editThisGame.location;
-	document.getElementById('dateEdit').value = editThisGame.date;
+	var docRef = db.collection("games").doc(localStorage.getItem('editGameID'));
 
-	if(editThisGame.status === 'home') {
-		homeBoolean = true;
-		awayBoolean = false;
-	} else {
-		homeBoolean = false;
-		awayBoolean = true;
-	}
+	docRef.get().then(function(doc) {
+		if (doc.exists) {
+			console.log("doc data: ", doc.data());
+			document.getElementById('oponentEdit').value = doc.data().oponent;
+			document.getElementById('locationEdit').value = doc.data().location;
+			document.getElementById('dateEdit').value = doc.data().date;
+
+			if(doc.data().status === 'home') {
+				homeBoolean = true;
+				awayBoolean = false;
+			} else {
+				homeBoolean = false;
+				awayBoolean = true;
+			}
+		} else {
+			console.log("None");
+		}
+	}).catch(function(error) {
+		console.log(error);
+	});
 }
 
 
@@ -196,12 +220,12 @@ function populateEdit() {
  */
 function editGameCurr(e) {
 	//Getting values from form
-	var currOponent = document.getElementById('oponentEdit').value;
-	var currLocation = document.getElementById('locationEdit').value;
-	var currDate = document.getElementById('dateEdit').value;
-	var gameId = currOponent + '|' + currDate;
-	var currStatus = document.getElementsByName('statusEdit');
-	var currStatusValue;
+	currOponent = document.getElementById('oponentEdit').value;
+	currLocation = document.getElementById('locationEdit').value;
+	currDate = document.getElementById('dateEdit').value;
+	gameId = currOponent + '|' + currDate;
+	currStatus = document.getElementsByName('statusEdit');
+	currStatusValue;
 	for(let i=0; i < currStatus.length; i++) {
 		if(currStatus[i].checked) {
 			currStatusValue = currStatus[i].value;
@@ -209,20 +233,28 @@ function editGameCurr(e) {
 	}
 
 	// Updating current entry
-	var currId = editThisGame.id;
-	var schedule = JSON.parse(localStorage.getItem('schedule'));
-	for (let i = 0; i < schedule.length; i++) {
-		if (schedule[i].id === currId) {
-			schedule[i].oponent = currOponent;
-			schedule[i].location = currLocation;
-			schedule[i].date = currDate;
-			schedule[i].status = currStatusValue;
-			schedule[i].id = currOponent + '|' + currDate;
-			break;
-		}
-	}
-	
-	localStorage.setItem('schedule', JSON.stringify(schedule));  
+	var game = {
+		oponent: currOponent,
+		location: currLocation,
+		date: currDate,
+		status: currStatusValue,
+		id: gameId
+	};
+
+	//Creating object to store in database
+	db.collection('games').doc(gameId).set(game).then(function() {
+		console.log('Success');
+	}).catch(function(error) {
+		console.error(error);
+	});
+
+	//deleting object
+	db.collection('games').doc(localStorage.getItem('editGameID')).delete().then(function() {
+    	console.log('Document successfully deleted!');
+	}).catch(function(error) {
+    	console.error('Error removing document: ', error);
+	});
+
 
 	//reseting form
 	gameToEdit.reset();
